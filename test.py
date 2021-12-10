e7669497d2e8a08c228c7904eeb53bc6976d71c5
Youssef Sayed <47215226+youssef893@users.noreply.github.com>
2021-12-10 14:00:29 +0200

    model

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import requests
 
-request = requests.post('https://youssefchatbot.herokuapp.com/webhooks/rest/webhook', json={"message": "youssef sayed ahmed"})
+request = requests.post('https://youssefchatbot.herokuapp.com/webhooks/rest/webhook', json={"message": "hello"})
 
 print("Bot says, ", end='')
 bot_message = ''
+print(request.json())
 for i in request.json():
 	bot_message = i['text']
 	print(f"{bot_message}")
