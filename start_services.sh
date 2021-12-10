6e2a197dafea44da5c1daf27580389ddd0ce0119
Youssef Sayed <47215226+youssef893@users.noreply.github.com>
2021-12-10 14:10:36 +0200

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,5 +1,4 @@
 cd app/
 # Start rasa server with nlu model
 rasa train
-rasa run --model models --endpoints endpoints.yml --credentials credentials.yml --enable-api --cors "*" --debug \
-          -p $PORT
+rasa run  
