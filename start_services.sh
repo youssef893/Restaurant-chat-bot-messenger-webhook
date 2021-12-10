99b90ea89bc1b0dd5768a6bddf5e7ca98cde3355
Youssef Sayed <47215226+youssef893@users.noreply.github.com>
2021-12-10 03:53:05 +0200

    Update start_services.sh

--- start_services.sh
+++ start_services.sh
@@ -1,3 +1,3 @@
 cd app/
 # Start rasa server with nlu model
-rasa run -m models --endpoints endpoints.yml --credentials credentials.yml
+rasa run -m Rasa-Restaurant-Bot/models --endpoints endpoints.yml --credentials credentials.yml
